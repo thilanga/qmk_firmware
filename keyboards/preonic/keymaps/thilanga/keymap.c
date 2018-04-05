fa01a3e78142568302a86a3ef4b4fab5a7c69dd3
Thilanga Pitigala <thilanga.pitigala@digital360.com.au>
2018-04-05 20:31:21 +1000

    Updated to add shift enter

--- keyboards/preonic/keymaps/thilanga/keymap.c
+++ keyboards/preonic/keymaps/thilanga/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Sftent|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      | Alt  | GUI  |Lower |SPACEDUAL |Raise |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,  \
   KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
   _______, _______, KC_LALT, KC_LGUI, LOWER,   SPACEDUAL,  RAISE,   _______, _______, _______, _______  \
 ),
 

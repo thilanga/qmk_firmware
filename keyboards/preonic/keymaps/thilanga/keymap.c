91edccf870d271aa3bade00e68c0b3aeecb2cc64
Thilanga Pitigala <thilanga.pitigala@digital360.com.au>
2018-04-05 20:26:03 +1000

    Updated to add dual space to handlle functions

--- keyboards/preonic/keymaps/thilanga/keymap.c
+++ keyboards/preonic/keymaps/thilanga/keymap.c
@@ -6,6 +6,9 @@
 #define _RAISE 2
 #define _FUNCTION 3
 
+// dual-role shortcuts
+# define SPACEDUAL LT(_FUNCTION, KC_SPACE)
+
 enum preonic_keycodes {
   COLEMAK = SAFE_RANGE,
   LOWER,
@@ -25,16 +28,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      | Alt  | GUI  |Lower |Space |  Fn  |Raise |      |      |      |      |
+ * |      |      | Alt  | GUI  |Lower |SPACEDUAL |Raise |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 
-[_COLEMAK] = LAYOUT_preonic_grid( \
+[_COLEMAK] = LAYOUT_preonic_mit( \
   KC_ESC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,  \
   KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-  _______, _______, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  MO(_FUNCTION),  RAISE,   _______, _______, _______, _______  \
+  _______, _______, KC_LALT, KC_LGUI, LOWER,   SPACEDUAL,  RAISE,   _______, _______, _______, _______  \
 ),
 
 /* Lower
@@ -50,12 +53,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |       |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT_preonic_grid( \
+[_LOWER] = LAYOUT_preonic_mit( \
   _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,   _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______,  _______ \
+  _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______,  _______ \
 ),
 
 /* Raise
@@ -71,12 +74,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = LAYOUT_preonic_grid( \
+[_RAISE] = LAYOUT_preonic_mit( \
   _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,  \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
 /* Function
@@ -92,12 +95,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_FUNCTION] = LAYOUT_preonic_grid( \
+[_FUNCTION] = LAYOUT_preonic_mit( \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
   _______, _______, KC_VOLD, KC_VOLU, KC__MUTE,KC_HOME, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_END, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 )
 
 };

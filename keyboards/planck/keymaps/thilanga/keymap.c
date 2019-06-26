1b950da7efc185fbb69875a3d4e9ff5fa39dffca
Thilanga Pitigala <pittado@gmail.com>
2019-06-27 09:47:47 +1000

    Updated to add ctrl - tab key

--- keyboards/planck/keymaps/thilanga/keymap.c
+++ keyboards/planck/keymaps/thilanga/keymap.c
@@ -41,6 +41,7 @@ enum preonic_keycodes {
 // dual-role shortcuts
 # define SPACELOWER LT(_LOWER, KC_SPACE)
 # define SPACERAISE LT(_RAISE, KC_SPACE)
+# define CTRL_CAD LCTL_T(KC_TAB)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -105,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Esc  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  "  |
+ * |Ctl/Tab|   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  "  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Sftent|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -114,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_COLEMAK] = LAYOUT_planck_2x2u(
     KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-    KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+    CTRL_CAD, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
     _______, _______, KC_LGUI, KC_LALT, SPACELOWER, SPACERAISE,   KC_RALT,   _______, _______,  KC_RCTL
 )
